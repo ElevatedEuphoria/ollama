eb256f0be120aacf37d877c0ab5a4f0c299c614e
Michael Yang <mxyng@pm.me>
2024-06-03 15:53:58 -0700

    add conversion for microsoft phi 3 mini/medium 4k, 128

--- convert/convert.go
+++ convert/convert.go
@@ -76,6 +76,8 @@ func Convert(d string, ws io.WriteSeeker) error {
 		c = &mixtral{}
 	case "GemmaForCausalLM":
 		c = &gemma{}
+	case "Phi3ForCausalLM":
+		c = &phi3{}
 	default:
 		return errors.New("unsupported architecture")
 	}
